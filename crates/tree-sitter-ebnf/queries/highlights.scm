6df41123340e5bbf581544d26a50974f84c496d6
RubixDev <silas.groh@t-online.de>
2022-10-02 17:13:34 +0200

    tree-sitter: Add highlight queries for PascalCase and camelCase names

--- crates/tree-sitter-ebnf/queries/highlights.scm
+++ crates/tree-sitter-ebnf/queries/highlights.scm
@@ -9,7 +9,11 @@
 
 ; Default to parameter
 (identifier) @parameter @grammar.nonterminal
-; Allow different highlighting for fully UPPERCASE and lowercase identifiers
+; Allow different highlighting for specific casings
+((identifier) @grammar.nonterminal.pascal
+ (#match? @grammar.nonterminal.pascal "^[A-Z]"))
+((identifier) @grammar.nonterminal.camel
+ (#match? @grammar.nonterminal.camel "^[a-z]"))
 ((identifier) @grammar.nonterminal.upper
  (#match? @grammar.nonterminal.upper "^[A-Z][A-Z0-9_]+$"))
 ((identifier) @grammar.nonterminal.lower
