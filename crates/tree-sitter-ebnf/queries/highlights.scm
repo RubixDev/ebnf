9709c79f4c4ca1f8576ea91aecb8c475f2963bda
RubixDev <silas.groh@t-online.de>
2022-10-02 17:33:51 +0200

    tree-sitter: Edit highlight queries

--- crates/tree-sitter-ebnf/queries/highlights.scm
+++ crates/tree-sitter-ebnf/queries/highlights.scm
@@ -1,24 +1,29 @@
+;;;; Simple tokens ;;;;
 (terminal) @string @grammar.terminal
 
-; Default to string.regex
-(special_sequence) @string.regex @grammar.other
+(special_sequence) @string.special @grammar.special
 
-(integer) @constant.numeric @number
+(integer) @number
 
 (comment) @comment.block
 
-; Default to parameter
-(identifier) @parameter @grammar.nonterminal
+;;;; Identifiers ;;;;
+(identifier) @variable @grammar.nonterminal
+
 ; Allow different highlighting for specific casings
 ((identifier) @grammar.nonterminal.pascal
  (#match? @grammar.nonterminal.pascal "^[A-Z]"))
+
 ((identifier) @grammar.nonterminal.camel
  (#match? @grammar.nonterminal.camel "^[a-z]"))
+
 ((identifier) @grammar.nonterminal.upper
  (#match? @grammar.nonterminal.upper "^[A-Z][A-Z0-9_]+$"))
+
 ((identifier) @grammar.nonterminal.lower
  (#match? @grammar.nonterminal.lower "^[a-z][a-z0-9_]+$"))
 
+;;; Punctuation ;;;;
 [
  ";"
  ","
